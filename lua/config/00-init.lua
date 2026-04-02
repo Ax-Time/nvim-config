25a4d06219e56ae22485c1f637ec6de7105a8beb
Giacomo Cartechini <giacomo1307@gmail.com>
2026-04-02 12:38:38 +0100

    feat: add 00-init.lua general settings

---
+++ lua/config/00-init.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.timeoutlen = 300
+opt.undofile = true
+opt.swapfile = false
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+opt.wrap = false
+opt.wildmenu = true
+opt.wildignore = "*.o,*.obj,*.pyc,*.class,*.build"
+opt.completeopt = "menu,menuone,noinsert"
+opt.ignorecase = true
+opt.smartcase = true
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+opt.termguicolors = true
+opt.showmode = false
+opt.cmdheight = 1
+opt.updatetime = 200
+opt.redrawtime = 1500
+
+vim.g.mapleader = " "
