e950e82b912c29264a73a59c0ca921dde3940e7c
Giacomo Cartechini <giacomo1307@gmail.com>
2026-04-02 12:52:16 +0100

    feat: add 08-ui.lua which-key and gitsigns

---
+++ lua/config/08-ui.lua
@@ -0,0 +1,27 @@
+require("which-key").setup()
+
+require("gitsigns").setup({
+  signs = {
+    add = { text = "▎" },
+    change = { text = "▎" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "┋" },
+  },
+  signcolumn = true,
+  numhl = false,
+  linehl = false,
+  word_diff = false,
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true,
+  },
+  attach_to_untracked = true,
+  current_line_blame = false,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = "eol",
+    delay = 1000,
+  },
+})
