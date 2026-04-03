fad6d47e4892f6ade3c1d550c0ba36ff310aadfa
Giacomo Cartechini <giacomo1307@gmail.com>
2026-04-03 16:16:04 +0100

    fix(lsp): wrap vim.lsp.buf.format in function for proper timeout handling

--- lua/config/04-lsp.lua
+++ lua/config/04-lsp.lua
@@ -17,7 +17,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
   vim.keymap.set("n", "gr", vim.lsp.buf.rename, opts)
-  vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, opts)
+  vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format({ timeout_ms = 10000 }) end, opts)
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 end
